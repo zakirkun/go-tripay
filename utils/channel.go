168d21dd8551124dd176ad3707dc9aea5e97da09
fanchann <fannyarifnass@gmail.com>
2024-04-15 22:10:42 +0700

    (add) : add channel code

---
+++ utils/channel.go
@@ -0,0 +1,26 @@
+package utils
+
+const (
+	CHANNEL_MYBVA          = "MYBVA"          //Maybank Virtual Account
+	CHANNEL_PERMATAVA      = "PERMATAVA"      // Permata VA
+	CHANNEL_BNIVA          = "BNIVA"          // BNI VA
+	CHANNEL_BRIVA          = "BRIVA"          // BRI VA
+	CHANNEL_MANDIRIVA      = "MANDIRIVA"      // MANDIRI VA
+	CHANNEL_BCAVA          = "BCAVA"          // BCA VA
+	CHANNEL_MUAMALATVA     = "MUAMALATVA"     // Muamalat Virtual Account
+	CHANNEL_CIMBVA         = "CIMBVA"         // CIMB Niaga Virtual Account
+	CHANNEL_BSIVA          = "BSIVA"          // BSI Virtual Account
+	CHANNEL_OCBCVA         = "OCBCVA"         // OCBC NISP Virtual Account
+	CHANNEL_DANAMONVA      = "DANAMONVA"      // Danamon Virtual Account
+	CHANNEL_OTHERBANKVA    = "OTHERBANKVA"    // Other Bank Virtual Account
+	CHANNEL_ALFAMART       = "ALFAMART"       // ALFAMART
+	CHANNEL_INDOMARET      = "INDOMARET"      // INDOMARET
+	CHANNEL_ALFAMIDI       = "ALFAMIDI"       // ALFAMIDI
+	CHANNEL_OVO            = "OVO"            // OVO
+	CHANNEL_QRIS           = "QRIS"           // QRIS by ShopeePay
+	CHANNEL_QRISC          = "QRISC"          //	QRIS (Customizable)
+	CHANNEL_QRIS2          = "QRIS2"          // QRIS
+	CHANNEL_DANA           = "DANA"           // DANA
+	CHANNEL_SHOPEEPAY      = "SHOPEEPAY"      // ShopeePay
+	CHANNEL_QRIS_SHOPEEPAY = "QRIS_SHOPEEPAY" //QRIS Custom by ShopeePay
+)
