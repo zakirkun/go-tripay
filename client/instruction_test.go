1fa274a5a1d862e87eeff2dbbbd9b0fbb2d4c4df
fanchann <fannyarifnass@gmail.com>
2024-05-06 13:50:03 +0700

    (test): - add merchant_pay - change intruction_test

--- client/instruction_test.go
+++ client/instruction_test.go
@@ -12,7 +12,7 @@ func TestInstruction(t *testing.T) {
 		MerchantCode: "T14302",
 		ApiKey:       "DEV-ZKIDl5gE3AsCDThj7mWX6yvQ8f42NZWJWlZ7TSzS",
 		PrivateKey:   "J2WTm-93avv-w0PZV-ur1t4-4TCjd",
-		Mode:         utils.MODE_DEVELOPMENT,
+		Mode:         utils.URL_DEVELOPMENT,
 	}
 
 	reponseOk, responseBad := client.Instruction("BRIVA", "", "10000", "")
